2fdfafb1fd428d447ddf34b4eeb03313e8ccabb2
narendrakorrapati <korrapatinarendra843@gmail.com>
2024-01-27 12:40:39 +0530

    First commit

---
+++ src/main/java/com/narendra/Oauth2WebClientApplication.java
@@ -0,0 +1,13 @@
+package com.narendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2WebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2WebClientApplication.class, args);
+	}
+
+}
